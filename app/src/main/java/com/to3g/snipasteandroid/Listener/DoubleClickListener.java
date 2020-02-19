5e2d121e69eaf5584a6b51e3971802ba36afeeb1
shengdi1995 <378978764@qq.com>
2020-02-19 20:14:28 +0800

    [Add] 剪贴文字。

---
+++ app/src/main/java/com/to3g/snipasteandroid/Listener/DoubleClickListener.java
@@ -0,0 +1,19 @@
+package com.to3g.snipasteandroid.Listener;
+
+import android.view.View;
+
+public  abstract class DoubleClickListener implements View.OnClickListener {
+    private static final long DOUBLE_TIME = 1000;
+    private static long lastClickTime = 0;
+
+    @Override
+    public void onClick(View v) {
+        long currentTimeMillis = System.currentTimeMillis();
+        if (currentTimeMillis - lastClickTime < DOUBLE_TIME) {
+            onDoubleClick(v);
+        }
+        lastClickTime = currentTimeMillis;
+    }
+
+    public abstract void onDoubleClick(View v);
+}
--- app/src/main/java/com/to3g/snipasteandroid/MainActivity.java
+++ app/src/main/java/com/to3g/snipasteandroid/MainActivity.java
@@ -1,35 +1,81 @@
 package com.to3g.snipasteandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.widget.LinearLayout;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.to3g.snipasteandroid.Listener.DoubleClickListener;
 import com.yhao.floatwindow.FloatWindow;
+import com.yhao.floatwindow.MoveType;
 import com.yhao.floatwindow.Screen;
 
 public class MainActivity extends AppCompatActivity {
     LinearLayout mFloatLayout;
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        showFloat();
+        prepare();
     }
 
-    private void showFloat () {
+    private void prepare () {
         LayoutInflater inflater = LayoutInflater.from(getApplication());
         mFloatLayout = (LinearLayout) inflater.inflate(R.layout.text_paste, null);
+
+        mFloatLayout.setOnClickListener(new DoubleClickListener() {
+            @Override
+            public void onDoubleClick(View v) {
+                FloatWindow.destroy();
+            }
+        });
+    }
+
+    public void ShowFloat () {
         FloatWindow
                 .with(getApplicationContext())
                 .setView(mFloatLayout)
-                .setWidth(100)
                 .setHeight(Screen.width, 0.2f)
                 .setX(100)
                 .setY(Screen.height, 0.3f)
                 .setDesktopShow(true)
+                .setMoveType(MoveType.active)
                 .build();
+    }
 
+    public static String getClipContent(Context context) {
+        ClipboardManager clipboardManager = (ClipboardManager) context.getSystemService(Context.CLIPBOARD_SERVICE);
+        assert clipboardManager != null;
+        ClipData clipData = clipboardManager.getPrimaryClip();
+        if (clipData != null && clipData.getItemCount() > 0) {
+            CharSequence text = clipData.getItemAt(0).getText();
+            if (text != null) {
+                return text.toString();
+            }
+
+        }
+        return null;
+    }
+
+    public void onButtonClick(View view) {
+        String content = getClipContent(this);
+        Log.d(TAG, "onButtonClick: " + content);
+        if (content != null) {
+            ShowFloat();
+            TextView textView = mFloatLayout.findViewById(R.id.textView);
+            textView.setText(content);
+        } else {
+            Toast.makeText(this, "剪贴板为空", Toast.LENGTH_SHORT).show();
+        }
     }
 }
