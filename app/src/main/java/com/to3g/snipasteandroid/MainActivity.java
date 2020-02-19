fdd9fd240c42b417e7841d3f0171b4e365c4ed2a
shengdi1995 <378978764@qq.com>
2020-02-19 17:13:33 +0800

    [Add] 悬浮窗测试成功。

--- app/src/main/java/com/to3g/snipasteandroid/MainActivity.java
+++ app/src/main/java/com/to3g/snipasteandroid/MainActivity.java
@@ -2,12 +2,34 @@ package com.to3g.snipasteandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+
+import com.yhao.floatwindow.FloatWindow;
+import com.yhao.floatwindow.Screen;
 
 public class MainActivity extends AppCompatActivity {
+    LinearLayout mFloatLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        showFloat();
+    }
+
+    private void showFloat () {
+        LayoutInflater inflater = LayoutInflater.from(getApplication());
+        mFloatLayout = (LinearLayout) inflater.inflate(R.layout.text_paste, null);
+        FloatWindow
+                .with(getApplicationContext())
+                .setView(mFloatLayout)
+                .setWidth(100)
+                .setHeight(Screen.width, 0.2f)
+                .setX(100)
+                .setY(Screen.height, 0.3f)
+                .setDesktopShow(true)
+                .build();
+
     }
 }
