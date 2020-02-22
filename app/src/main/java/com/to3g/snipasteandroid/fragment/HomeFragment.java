b090dd3a4eeb7537820c89ce57516759b1d6cee1
shengdi1995 <378978764@qq.com>
2020-02-22 16:02:37 +0800

    [Add] 贴剪贴板文字。

--- app/src/main/java/com/to3g/snipasteandroid/fragment/HomeFragment.java
+++ app/src/main/java/com/to3g/snipasteandroid/fragment/HomeFragment.java
@@ -1,14 +1,10 @@
 package com.to3g.snipasteandroid.fragment;
 
-import android.content.ClipData;
-import android.content.ClipboardManager;
 import android.content.Context;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,6 +19,8 @@ import com.to3g.snipasteandroid.base.BaseFragment;
 import com.to3g.snipasteandroid.lib.ClipBoardUtil;
 import com.to3g.snipasteandroid.lib.Group;
 import com.to3g.snipasteandroid.lib.annotation.Widget;
+
+import java.util.Objects;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -42,44 +40,69 @@ public class HomeFragment extends BaseFragment {
     @BindView(R.id.pasteTextButton)
     QMUIRoundButton pasteTextButton;
 
+    @BindView(R.id.pasteClipboardButton)
+    QMUIRoundButton pasteClipboardButton;
+
     @BindView(R.id.topbar)
     QMUITopBarLayout mTopBar;
 
     @Override
     protected View onCreateView() {
+        // 绑定视图
         View root = LayoutInflater.from(getContext()).inflate(R.layout.home_layout, null);
         ButterKnife.bind(this, root);
+        // 初始化TopBar
         initTopBar();
+        // 绑定按钮点击事件
+        pasteClipboardButton.setOnClickListener(v -> {
+            onPasteClickboardButtonClick();
+        });
         pasteTextButton.setOnClickListener(v -> {
-            String content = ClipBoardUtil.get(getContext());
-            Log.d(TAG, "剪切板内容: " + content);
-            editText.setText(content);
-
-//            LayoutInflater inflater = LayoutInflater.from(getActivity());
-//            LinearLayout mFloatLayout = (LinearLayout) inflater.inflate(R.layout.text_paste, null);
-//
-//            TextView textView = mFloatLayout.findViewById(R.id.textView);
-//            textView.setText(content);
-
-            EasyFloat
-                    .with(getActivity())
-                    .setLayout(R.layout.text_paste)
-                    .setShowPattern(ShowPattern.ALL_TIME)
-                    .setLocation(100, 200)
-                    .setTag(content)
-                    .show();
-
-            TextView textView1 = EasyFloat.getAppFloatView(content).findViewById(R.id.textView);
-            EasyFloat.getAppFloatView(content).findViewById(R.id.textView).setOnClickListener(new DoubleClickListener() {
-                @Override
-                public void onDoubleClick(View v) {
-                    EasyFloat.dismissAppFloat(content);
-                }
-            });
-            textView1.setText(content);
-
+            onPasteTextButtonClick();
         });
         return root;
+    }
+
+    private void onPasteTextButtonClick () {
+        floatText(editText.getText().toString());
+    }
+
+    private void onPasteClickboardButtonClick () {
+        String content = ClipBoardUtil.get(getContext());
+        Log.d(TAG, "剪切板内容: " + content);
+        editText.setText(content);
+        floatText(content);
+    }
+
+    private void floatText(String content) {
+        View view = EasyFloat.getAppFloatView(content);
+        if (content == null || content.equals("")) {
+            Toast.makeText(getContext(), "内容为空", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        // 如果已经创建，返回 false
+        if (view != null) {
+            Toast.makeText(getContext(), "已悬浮", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        // 创建一个新的
+        EasyFloat
+                .with(Objects.requireNonNull(getActivity()))
+                .setLayout(R.layout.text_paste)
+                .setShowPattern(ShowPattern.ALL_TIME)
+                .setLocation(100, 200)
+                .setTag(content)
+                .show();
+        view = EasyFloat.getAppFloatView(content);
+        assert view != null;
+        TextView textView = view.findViewById(R.id.textView);
+        view.findViewById(R.id.textView).setOnClickListener(new DoubleClickListener() {
+            @Override
+            public void onDoubleClick(View v) {
+                EasyFloat.dismissAppFloat(content);
+            }
+        });
+        textView.setText(content);
     }
 
     private void initTopBar() {
